6bec56678a48adf94545397161854f0fb294170e
Fumiya Sakai <just1factory@gmail.com>
2021-01-15 11:44:41 +0900

    initial commit

---
+++ AndroidCatchUpExampleServerSideKotlin/src/main/kotlin/net/just1factory/AndroidCatchUpExampleServerSideKotlin/AndroidCatchUpExampleServerSideKotlinApplication.kt
@@ -0,0 +1,11 @@
+package net.just1factory.AndroidCatchUpExampleServerSideKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AndroidCatchUpExampleServerSideKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<AndroidCatchUpExampleServerSideKotlinApplication>(*args)
+}
---
+++ AndroidCatchUpExampleServerSideKotlin/src/test/kotlin/net/just1factory/AndroidCatchUpExampleServerSideKotlin/AndroidCatchUpExampleServerSideKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.just1factory.AndroidCatchUpExampleServerSideKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AndroidCatchUpExampleServerSideKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
